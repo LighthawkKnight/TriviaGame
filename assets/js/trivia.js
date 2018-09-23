360e42de3c7892e243fa6f5c5e1dc7ea52691778
LighthawkKnight <lighthawkknight@gmail.com>
2018-09-23 00:56:08 -0700

    Added second quiz

--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -10,7 +10,15 @@ const gameQuestions = [ "How many pokemon are there total as of Ultra Sun/Moon?"
     "What pokemon nature increases Speed but decreases Special Attack?"];
 
 const animeQuestions = ["What was Pikachu's first Z-Move?",
-                        "In Pikachu language, what is Ash's name?"]
+                        "In Pikachu language, what is Ash's name?",
+                        "Ash usually gets the starters in each region he's been to, but which one of these has he never owned?",
+                        "Among whom of all of Ash's companions had the longest tenure traveling with him?",
+                        "While Ash has fallen short of the top of the podium many times, which League Championship has he placed 1st in?",
+                        "Which pokemon is infamous for an incident involving casuing seizures in young children?",
+                        "Out of all of Ash's female traveling companions, who is the only one that was expressly invited by Ash to join him?",
+                        "Of Ash's original team of 6, which one of them has either not evolved nor refused to evolve?",
+                        "Who was James's (of Team Rocket) first pokemon?",
+                        "Of these five moves that Charizard can learn, which of these moves has Ash's Charizard never used?"]
 
 // Multiple choice options indexed by the same order as the questions
 const gameMultipleChoiceArr = [
@@ -28,12 +36,20 @@ const gameMultipleChoiceArr = [
 const animeMultipleChoiceArr = [
     ["Gigavolt Havoc", "Breakneck Blitz", "10,000,000 Volt Thunderbolt", "Catastropika", "Stoked Sparksurfer"],
     ["Pikachu-Pi", "PikaPika", "PikaPi", "Pi-Pikachu", "PikaPika"],
-    [""]
-    ]
-]
+    ["Totodile", "Treecko", "Froakie", "Piplup", "Oshawott"],
+    ["Misty", "Brock", "Dawn", "Cilan", "Max"],
+    ["Indigo Plateau", "Orange League", "Silver Conference", "Lily of the Valley", "Lumios Conference"],
+    ["Porygon", "Magneton", "Electrode", "Poliwhirl", "Hypno"],
+    ["Misty", "May", "Dawn", "Iris", "Serena"],
+    ["Pikachu", "Pidgeotto", "Bulbasaur", "Charmander", "Squirtle"],
+    ["Koffing", "Weezing", "Victreebell", "Growlithe", "Carnivine"],
+    ["Steel Wing", "Dragon Tail", "Overheat", "Blast Burn", "Seismic Toss"]];
+    
+
 
 // The correct answer indexed by the same order as above
 const gameCorrectAnswer = ["807", "151", "8", "Flash", "Blue", "No Guard", "Ruby/Sapphire/Emerald", "Xerneas/Yveltal", "Ghost replaces Flying", "Jolly"];
+const animeCorrectAnswer = ["Gigavolt Havoc", "PikaPi", "Piplup", "Brock", "Orange League", "Porygon", "Serena", "Squirtle", "Growlithe", "Blast Burn"];
 // Some trivia to go along with the correct answer, indexed
 const gameTrivia = ["It's going to break 4 digits soon enough.  Can you name them all?",
                 "If you counted 150, I hope you didn't forget cute little Mew!",
@@ -45,10 +61,32 @@ const gameTrivia = ["It's going to break 4 digits soon enough.  Can you name the
                 "Xerneas/Yveltal were the legendary exclusives for X/Y respectively, both of whom were pivotal to the main story.",
                 "Hard to believe the swift ninja-like Decidueye evolves from this cute little owl pokemon.",
                 "Timid, Hasty, and Naive increased your speed as well, but sacrificed other stats."]
+const animeTrivia = ["Pikachu has learned 3 different Z-Moves so far, but he learned Gigavolt Havoc very early when Z-Moves were introduced.",
+                    "'PikaPi' sounds very similar to 'Satoshi', Ash's original JP name.",
+                    "While Piplup had plenty of screentime, it belonged to Dawn, his main female companion in Sinnoh.",
+                    "Brock has traveled with Ash through 4 different regions, his tenure as a protagonist second only to Ash and Pikachu.",
+                    "It was well earned for sure, but also kind of depressing that the only championship he won was an anime-original league.",
+                    "The Porygon episode had a 4 second scene where some blown up virtual missiles caused a fast flickering light effect taking up most of the screen.",
+                    "The other four self-proclaimingly invited themselves in for quite a wide array of reasons, although they all became very close friends with him eventually.",
+                    "Both Charmander and Pidgeotto evolved, while Bulbasaur and Pikachu when given the opportunity refused to evolve.",
+                    "Growlie, as he was affectionately nicknamed, was James's only friend as a young boy, who has remained loyal to him ever since.",
+                    "Despite that, Ash's Charizard has grown to be extremely powerful both in physical and fire attacks.  Maybe one day he'll finally mega-evolve him!"]
 // Image accompanying answer indexed by the same order as above
 const p = "assets/img/"  // Image path
 const gameCorrectImg = [p+"807pokemon.gif", p+"mew.gif",p+"eeveelutions.gif",p+"hm05flash.png",p+"gary.gif",p+"machamp.gif",
                     p+"delia.gif",p+"xylegendaries.gif",p+"rowlet.gif", p+"totodile.gif"];
+const animeCorrectImg = [
+    /*Z-Move*/"https://78.media.tumblr.com/764a0f8750e80e5878b9ea4d5ca6beaa/tumblr_inline_ogtewbr0eI1tvvsht_500.gif",
+    /*Pikachu*/"https://i.pinimg.com/originals/b4/51/cf/b451cf45633cbc4426f20d92efeb5615.gif",
+    /*Piplup*/"https://media.giphy.com/media/uLofTbBSg3lQI/source.gif",
+    /*Brock*/"https://78.media.tumblr.com/4e68714d5af92efb971efacde250a236/tumblr_owalsoS7B51w3p557o2_500.gif",
+    /*Trophy*/"https://i.imgur.com/hC8xjjK.gif",
+    /*Porygon*/"https://78.media.tumblr.com/48d8f271f9701965b5bd18f53c2940d5/tumblr_oacmijjqon1thhq7eo1_400.gif",
+    /*Serena*/"https://78.media.tumblr.com/e97226b89b16895b8d23cdb9bae5eec8/tumblr_o5md7epf0g1thhi41o1_500.gif",
+    /*Squirtle*/"https://i.imgur.com/oUhCd5p.gif",
+    /*Growlie*/"http://images6.fanpop.com/image/photos/32200000/Growlie-and-James-growlie-jamess-growlithe-32244842-500-300.gif",
+    /*Charizard*/"https://media.giphy.com/media/iS6w9L0bYq1Ne/giphy.gif"];
+
 // Time limits
 const questionTime = 25;
 const resultTime = 3;
@@ -150,11 +188,11 @@ function start(){
             $('#animeQuiz').fadeOut('fast');
         }
         else if (choice === "animeQuiz") {
-            questions = gameQuestions;
-            multipleChoiceArr = gameMultipleChoiceArr;
-            correctAnswer = gameCorrectAnswer;
-            trivia = gameTrivia;
-            correctImg = gameCorrectImg;
+            questions = animeQuestions;
+            multipleChoiceArr = animeMultipleChoiceArr;
+            correctAnswer = animeCorrectAnswer;
+            trivia = animeTrivia;
+            correctImg = animeCorrectImg;
             $('#gameQuiz').fadeOut('fast');
         }
 
@@ -262,7 +300,7 @@ function gameOver() {
     $('#gameOver').show();
     $('#right').html(" " + right);
     $('#wrong').html(" " + wrong);
-    $('#unaswered').html(" " + timedOut)
+    $('#unanswered').html(" " + timedOut)
 }
 
 // This method of shuffling goes through each index of the array once and randomly swaps it with another.  Returns the same array passed in, except shuffled
